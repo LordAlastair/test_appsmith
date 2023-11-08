99ac55964d27c40acfe78704e6715724c8efd3ee
LordAlastair <lordalastair>
2023-11-08 18:27:42 +0000

    System generated commit, initial commit

---
+++ pages/Page1/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,8 @@
+export default {
+	myVar1: [{"Tipo":"ALI","Baixa":"7","Media":"10","Alta":"15"},{"Tipo":"AIE","Baixa":"5","Media":"7","Alta":"10"},{"Tipo":"EE","Baixa":"3","Media":"4","Alta":"6"},{"Tipo":"SE","Baixa":"4","Media":"5","Alta":"7"},{"Tipo":"CE","Baixa":"3","Media":"5","Alta":"6"}],
+
+	myFun1 () {
+		return	this.myVar1;
+	},
+
+}
